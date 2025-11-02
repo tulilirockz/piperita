20013150a74f98955e43f2c39f12caeeb6b4e1e9
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-02 01:27:22 +0000

    Steam already there

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -xeuo pipefail
 
 # Installing steam updates something that fixes the kernel
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf -y install steam
 
 for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
   rpm --erase $pkg --nodeps
@@ -22,7 +21,6 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-a
   scx-scheds-git \
   scx-manager
 
-dnf -y remove steam
 rm -rf /etc/yum.repos.d/rpmfusion*
 
 tee /etc/modules-load.d/ntsync.conf <<'EOF'
