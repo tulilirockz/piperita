78998305772ea68e8ec694e175e6880594db34fe
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-02 11:08:21 +0000

    I have to figure out how to enable secure boot for this kernel

--- build.sh
+++ build.sh
@@ -46,5 +46,6 @@ ntsync
 EOF
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
-dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+export DRACUT_NO_XATTR=1
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
