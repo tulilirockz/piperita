c8b5f8e47fb269633527f108b40c9d26ff2c2211
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-02 11:50:21 +0000

    Lets try to be cleaner

--- build.sh
+++ build.sh
@@ -2,17 +2,15 @@
 
 set -xeuo pipefail
 
-# create a shims to bypass kernel install triggering dracut/rpm-ostree
-# seems to be minimal impact, but allows progress on build
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
+
 pushd /usr/lib/kernel/install.d
-mv 05-rpmostree.install 05-rpmostree.install.bak
-mv 50-dracut.install 50-dracut.install.bak
 printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
 printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
 chmod +x  05-rpmostree.install 50-dracut.install
 popd
-
-dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
 dnf -y copr enable bieszczaders/kernel-cachyos
 dnf -y copr disable bieszczaders/kernel-cachyos
@@ -25,13 +23,12 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-a
 dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
   scx-manager
 
-rm -rf /etc/yum.repos.d/rpmfusion*
-
 tee /etc/modules-load.d/ntsync.conf <<'EOF'
 ntsync
 EOF
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
-dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
