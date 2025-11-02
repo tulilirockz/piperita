68cd354a3c072bcc25e7a5c552edfebdf490fed5
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-02 20:55:15 +0000

    Cleaning house again

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -xeuo pipefail
-
-for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
-  rpm --erase $pkg --nodeps
-done
+set -ouex pipefail
 
 pushd /usr/lib/kernel/install.d
 printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
