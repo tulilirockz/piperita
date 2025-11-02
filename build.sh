63db9d325684c965b1eaa7bf6efba4a84917f0ff
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-02 21:06:27 +0000

    oopsieee

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -ouex pipefail
 
 pushd /usr/lib/kernel/install.d
+mv 05-rpmostree.install 05-rpmostree.install.bak
+mv 50-dracut.install 50-dracut.install.bak
 printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
 printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
 chmod +x  05-rpmostree.install 50-dracut.install
