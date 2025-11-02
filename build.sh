deb78aff31ecd442587b879a379330e15c36fd6d
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-02 10:52:33 +0000

    Could this fix secure boot

--- build.sh
+++ build.sh
@@ -45,7 +45,6 @@ tee /etc/modules-load.d/ntsync.conf <<'EOF'
 ntsync
 EOF
 
-QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel${KERNEL_SUFFIX:+-${KERNEL_SUFFIX}}")"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
