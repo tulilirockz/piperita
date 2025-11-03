c1739ed26a9a669ea4ed29a33de53aad254527cd
KitKat <227429303+kitkat6464@users.noreply.github.com>
2025-11-03 17:35:34 +0000

    Removing junk, my girl j .. nevermind

--- build.sh
+++ build.sh
@@ -9,25 +9,17 @@ for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
 done
 
 pushd /usr/lib/kernel/install.d
-mv 05-rpmostree.install 05-rpmostree.install.bak
-mv 50-dracut.install 50-dracut.install.bak
 printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
 printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
 chmod +x  05-rpmostree.install 50-dracut.install
 popd
 
-rm -r -f /usr/lib/modules
 dnf -y copr enable bieszczaders/kernel-cachyos
 dnf -y install kernel-cachyos
 
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
 dnf -y swap zram-generator-defaults cachyos-settings
 dnf -y install scx-manager
-
-pushd /usr/lib/kernel/install.d
-mv -f 05-rpmostree.install.bak 05-rpmostree.install
-mv -f 50-dracut.install.bak 50-dracut.install
-popd
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
