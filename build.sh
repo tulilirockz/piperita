8b7ee7f3e1d1ffb26ee7ec6d4f350334d62e4d05
Rin Koito <mouichidouryuuseigun@gmail.com>
2026-01-10 15:05:39 -0400

    fix: add depmod before dracut

--- build.sh
+++ build.sh
@@ -44,5 +44,6 @@ EOF
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
+depmod -a "$(ls -1 /lib/modules/ | tail -1)"
 dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
